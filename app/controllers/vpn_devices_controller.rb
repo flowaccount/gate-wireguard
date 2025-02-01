d6a23a897816ef31f6764ccf9836d6e202a465eb
NamSupawan <60772519+NamSupawan@users.noreply.github.com>
2025-02-01 10:40:25 +0700

    redirect

--- app/controllers/vpn_devices_controller.rb
+++ app/controllers/vpn_devices_controller.rb
@@ -51,11 +51,11 @@ class VpnDevicesController < ApplicationController
   def create
     config_file = params[:config_file]
     output, status = Open3.capture2e("sudo wg-quick up #{config_file}")
-
+    redirect_to :back
     #if status.success?
-      redirect_to new_vpn_device_path, notice: 'WireGuard interface created successfully.'
+    #redirect_to vpn_devices_path, notice: 'WireGuard interface created successfully.'
     #else
-      #redirect_to new_vpn_device_path, alert: "Failed to create WireGuard interface:\n#{output}"
+    #redirect_to new_vpn_device_path, alert: "Failed to create WireGuard interface:\n#{output}"
     #end
   end
 
