6d72fe1a5898a9f0ad4558c642e64618211bad8a
NamSupawan <60772519+NamSupawan@users.noreply.github.com>
2025-02-02 22:16:17 +0700

    url path

--- app/controllers/firewalls_controller.rb
+++ app/controllers/firewalls_controller.rb
@@ -22,7 +22,7 @@ class FirewallsController < ApplicationController
   def create
     @firewall = Firewall.new(firewall_params(@rule_name))
     #@firewall.name = @rule_name
-    if @firewall.name.nil? || @firewall.ipAddress.nil?
+    if @firewall.name.nil || @firewall.ipAddress.nil
       render plain: "Value Cannot Be null", status: :ok
     else
       output, status = Open3.capture2e("sudo ipset add #{@firewall.name} #{@firewall.ipAddress}")
