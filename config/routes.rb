557b7fe32da052d5ccf8d95caa058a035dd3d983
NamSupawan <60772519+NamSupawan@users.noreply.github.com>
2025-02-02 16:23:15 +0700

    firewall

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
   get 'auth/failure', to: redirect('/')
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'admin/users'
-  get 'firewall/rules', to: 'firewall#rules'
 
   patch 'admin/user/:id', to: 'admin#update_users_admin', as: 'update_users_admin'
 
