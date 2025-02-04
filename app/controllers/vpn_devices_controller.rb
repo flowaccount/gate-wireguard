8394d823ce545d681815045e8cdf07fc1a739395
NamSupawan <60772519+NamSupawan@users.noreply.github.com>
2025-02-04 14:01:01 +0700

    peer

--- app/controllers/vpn_devices_controller.rb
+++ app/controllers/vpn_devices_controller.rb
@@ -50,8 +50,9 @@ class VpnDevicesController < ApplicationController
   # POST /vpn_devices or /vpn_devices.json
   def create
     config_file = params[:config_file]
-    output, status = Open3.capture2e("sudo wg-quick up #{config_file}")
+    output, status = Open3.capture2e("sudo wg-quick up #{config_file}")   
     if status.success?
+      system("sudo systemctl restart wg-quick@wg0.service")
       redirect_to vpn_devices_path, notice: 'WireGuard interface created successfully.'
     else
       redirect_to new_vpn_device_path, alert: "Failed to create WireGuard interface:\n#{output}"
