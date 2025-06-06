5493aea9ead819ea32cba2227d7ba122d3fd7727
NamSupawan <60772519+NamSupawan@users.noreply.github.com>
2025-06-06 14:41:43 +0700

    userId

--- app/controllers/vpn_devices_controller.rb
+++ app/controllers/vpn_devices_controller.rb
@@ -50,7 +50,7 @@ class VpnDevicesController < ApplicationController
 
   # GET /vpn_devices/new
   def add_with_user
-    @vpn_device.user.id = params[:userId]
+    @vpn_device.user_id = params[:userId]
     @vpn_device.description = params[:description]
     @vpn_device.setup_device_with_keys
 
